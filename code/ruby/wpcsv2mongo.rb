5fa0a2a585338b03f5d6a95c67e056f2accbbb5e
neilfws <neilfws@gmail.com>
2010-12-22 01:08:24 +1100

    Change to better_keys

--- code/ruby/wpcsv2mongo.rb
+++ code/ruby/wpcsv2mongo.rb
@@ -11,6 +11,12 @@ data = "#{ENV['HOME']}/Dropbox/projects/birds/data/ausbirds.csv"
 FasterCSV.read(data, :headers => true).each do |record|
   record = record.to_hash
   record['_id']  = record['Binomial']
+  record['common_name'] = record['Common name']
+  record.delete('Common name')
+  record['binomial'] = record['Binomial']
+  record.delete('Binomial')
+  record['notes'] = record['Notes']
+  record.delete('Notes')
   record['seen'] = 0
   col.save(record)
 end
