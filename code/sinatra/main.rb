58e101f4e06244d7726b307c0cce3e9c25f3934c
neilfws <neilfws@gmail.com>
2011-01-05 18:57:08 +1100

    Code and View for top 20 genera by percent seen

--- code/sinatra/main.rb
+++ code/sinatra/main.rb
@@ -57,3 +57,12 @@ get "/extinct" do
   @extinct = options.species.find(:notes => /extinct/)
   haml :extinct
 end
+
+get "/genera" do
+  @genera = options.species.find.map {|s| s}.inject(Hash.new(0)) {|h, g| h[g['binomial'].split(" ")[0]] += 1; h}.to_a
+  @seen  = options.species.find.map {|s| s}.inject(Hash.new(0)) {|h, g| h[g['binomial'].split(" ")[0]] += g['seen']; h}.to_a
+  0.upto(@genera.count - 1) { |i| @genera[i][2] = @seen[i][1]}
+  @genera.map! {|e| [e[0], e[1] - e[2], e[2]]}
+  @genera = @genera.sort_by { |e| e[2].to_f/e[1]}.reverse[0..19]
+  haml :genera
+end
